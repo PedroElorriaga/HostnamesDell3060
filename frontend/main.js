0c0b907edd4589a5256de55ae7cd96be31a876fa
PedroElorriaga <pedrohes2002@outlook.com>
2023-07-03 21:11:05 -0300

    Resolução de exibição logado

--- frontend/main.js
+++ frontend/main.js
@@ -1,8 +1,10 @@
 import './assets/css/style.css';
 
 const status = document.querySelectorAll('.status')
+const hostInfo = document.querySelectorAll('.host-info')
+const logado = document.querySelector('.logado')
 
-for(let i = 0; i < status.length;i ++){
+for (let i = 0; i < status.length; i++) {
     let temp = status[i]
     if (temp.innerHTML === "REMOVIDO") {
         temp.style.color = '#32CD32'
@@ -10,5 +12,14 @@ for(let i = 0; i < status.length;i ++){
         temp.style.color = 'red'
     }
 }
+
+if (!logado) {
+    for (let i = 0; i < hostInfo.length; i++) {
+        let temp = hostInfo[i]
+        temp.setAttribute('hidden', '')
+    }
+}
+
+
 
 
