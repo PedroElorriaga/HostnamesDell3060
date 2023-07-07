c18645657fd5f6fca919cd01b9b9aa69aa42cefb
PedroElorriaga <pedrohes2002@outlook.com>
2023-07-06 23:19:31 -0300

    Testes de estilos

--- frontend/main.js
+++ frontend/main.js
@@ -3,6 +3,8 @@ import './assets/css/style.css';
 const status = document.querySelectorAll('.status')
 const hostInfo = document.querySelectorAll('.host-info')
 const logado = document.querySelector('.logado')
+const errorSn = document.querySelector('.sn')
+const errorMac = document.querySelector('.mac')
 
 for (let i = 0; i < status.length; i++) {
     let temp = status[i]
@@ -19,7 +21,3 @@ if (!logado) {
         temp.setAttribute('hidden', '')
     }
 }
-
-
-
-
